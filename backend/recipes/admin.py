88ec16f871f5ad686c95918dec2d560a94caaede
Lina Ivanova <shershlina@ya.ru>
2023-04-24 08:56:52 +0700

    small review changes

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import display
+from django.utils.safestring import mark_safe
 
 from .models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                      ShoppingCart, Tag)
@@ -18,9 +19,9 @@ class IngredientInLine(admin.TabularInline):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'id', 'author', 'added_in_favorites',
-                    'list_of_ingredients', 'list_of_tags')
+                    'list_of_ingredients', 'list_of_tags', 'image')
     readonly_fields = ('added_in_favorites', 'list_of_ingredients',
-                       'list_of_tags')
+                       'list_of_tags', 'image')
     list_filter = ('author', 'name', 'tags',)
     inlines = (TagInLine, IngredientInLine,)
 
@@ -39,6 +40,13 @@ class RecipeAdmin(admin.ModelAdmin):
         return ', '.join([
             tags.name for tags
             in obj.tags.all()])
+
+    @display(description='Изображение')
+    def image(self, obj):
+        if obj.image.exists():
+            return mark_safe(f'<img src={obj.image.url} '
+                             f'width="80" height="60">')
+        return None
 
 
 @admin.register(Ingredient)
